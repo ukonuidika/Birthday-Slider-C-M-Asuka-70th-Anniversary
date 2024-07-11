22b94a4e4210cd08818e5b078abca35123030d00
ukonuidika <120014847+ukonuidika@users.noreply.github.com>
2024-07-11 15:16:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
